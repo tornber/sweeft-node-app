cb8d1200074f626ed2f98f233d811a1befeec069
tornber <tornike.beridze264@gmail.com>
2023-03-24 18:53:09 +0400

    filtering outcomes added

--- models/category.js
+++ models/category.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
 const CategorySchema = new mongoose.Schema({
-    name: {type: String,required: true},
-    incomes: Array,
-    outcome: Number,
+    name: {type: String,required: [true,"username should be unique"],trim: true},
+    incomes: [{
+        description: {type: String,required: true},
+        amount: {type: Number,required: true},
+        default: {}
+    }],
+    outcomes: [{
+        description: {type: String,required: true},
+        amount: {type: Number,required: true},
+        status: {type: String,enum: ['Processing','Completed'],default: 'Processing',required: true},
+        default: {}
+    }],
+    date: {type:Date,default: Date.now},
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const User = require('./models/user')
 const Category = require('./models/category')
 const jwb = require('jsonwebtoken')
 require('dotenv').config()
-//ff
+
 const JWBSECRET = process.env.SECRET
 
 mongoose.connect('mongodb://localhost:27017/sweeftApp',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     autoIndex: true
 })
 
@@ -34,7 +36,7 @@ app.post('/register',async (req,res) => {
     try {
         const user = await User.create({
             username,
-            password
+            password: hashedPassword
         })
         const jwbToken = jwb.sign({id: user._id,username: user.username},JWBSECRET)
         return res.status(201).json({status: 'ok',data: user,token: jwbToken})
@@ -49,11 +51,11 @@ app.post('/login',async (req,res) => {
     const {username,password} = req.body 
     const user = await User.findOne({username})
     
-    if(!user) {
+    if (!user) {
         return res.status(401).json({status: 'error',message: 'invalid username/password'})
     }
 
-    if(bcrypt.compare(password,user.password)) {
+    if (await bcrypt.compare(password,user.password)) {
         const jwbToken = jwb.sign({id: user._id,username: user.username},JWBSECRET)
         return res.status(200).json({status: 'ok',token:jwbToken})
     }
@@ -62,15 +64,35 @@ app.post('/login',async (req,res) => {
 })
 
 app.put('/change-password',async (req,res) => {
-    const {token,password} = req.body 
+    
+    const {token,password,oldPassword} = req.body 
+    if (!password || !oldPassword) {
+        return res.status(400).json({status: 'error',message: "some of the fields is not filled"})
+    }
+    if (password.length === 0 || password.length > 10) {
+        return res.status(400).json({status: 'error',message: "password can't be empty or more than 10 characters long"})
+    }
+    let user
     try {
-        const user = jwb.verify(token,JWBSECRET)
-        if(user.hasOwnProperty('id')) {
+        user = jwb.verify(token,JWBSECRET)
+    } catch(error) {
+        return res.status(403).json({status: 'error',message: 'invalid session, invalid json web token format'})
+    }
+    try {
+        if(!user.hasOwnProperty('id')) {
             return res.status(403).json({status: 'error',message: 'invalid session'})
         }
         const id = user.id 
+        const databaseUser = await User.findById(id)
+        const isOldPasswordCorrect = await bcrypt.compare(oldPassword,databaseUser.password)
+        if(!isOldPasswordCorrect) {
+            return res.status(400).json({status: "error",message: "invalid old password"})
+        }
+        if (await bcrypt.compare(password,databaseUser.password)) {
+            return res.status(400).json({status: 'error',message: 'password is same with new old one'})
+        }
         const hashedPassword = await bcrypt.hash(password,10)
-        await User.updateOne({id},{
+        await User.updateOne({_id: id},{
             $set: {password: hashedPassword}
         })
         return res.status(204).send()
@@ -84,26 +106,51 @@ app.post('/new-category',async (req,res) => {
         return res.status(415).json({status: 'error',message: 'unsupported media type'})
     }
     const {name,token} = req.body
-    const user = jwb.verify(token,JWBSECRET)
-    if (!name) {
-        return res.status(422).json({status: 'error',message: 'category name required'})
+    let user
+    try {
+        user = jwb.verify(token,JWBSECRET)
+    } catch(error) {
+        return res.status(400).json({status: 'error',message: 'invalid token format'})
     }
     try {
+        if (!name) {
+            return res.status(400).json({status: 'error',message: "category name required"})
+        }
+        const category = await Category.findOne({name,userId: user.id})
+        if(category) {
+            return res.status(400).json({status: 'error',message: `category with name ${name} is already created`})
+        }
         await Category.create({name,userId: user.id})
         return res.status(200).json({status: 'ok',message: 'category created'})
 
     } catch(error) {
+        console.log(error)
         return res.status(500).json({status: 'error',message: 'internal server error'})
     }
 }) 
 
 app.put('/update-category',async (req,res) => {
     const {name,token,newName} = req.body
-    const user = jwb.verify(token,JWBSECRET)
+    let user
+    try {
+        user = jwb.verify(token,JWBSECRET)
+    } catch(error) {
+        return res.status(400).json({status: 'error',message: 'invalid token format'})
+    }
     if (!name) {
-        return res.status(422).json({status: 'error',message: 'category name required'})
+        return res.status(400).json({status: 'error',message: 'category old name required'})
+    }
+    if (!newName) {
+        return res.status(400).json({status: 'error',message: 'category new name required'})
+    }
+    if(name === newName) {
+        return res.status(400).json({status: 'error',message: 'new name should be different from old one'})
     }
     try {
+        const exists = await Category.exists({name,userId: user.id})
+        if (!exists) {
+            return res.status(400).json({status: 'error',message: 'invalid category name'})
+        }
         const {_id} = await Category.findOne({name,userId: user.id})
         await Category.findByIdAndUpdate(_id,{
             $set: {name: newName}}
@@ -116,42 +163,90 @@ app.put('/update-category',async (req,res) => {
 
 app.delete('/delete-category',async (req,res) => {
     const {name,token} = req.body
-    const user = jwb.verify(token,JWBSECRET)
+    let user
+    try {
+        user = jwb.verify(token,JWBSECRET)
+    } catch(error) {
+        return res.status(400).json({status: 'error',message: 'invalid token format'})
+    }
     if (!name) {
-        return res.status(422).json({status: 'error',message: 'category name required'})
+        return res.status(400).json({status: 'error',message: 'category name required'})
     }
     try {
-        const {income,outcome,userId} = await Category.findOne({name,userId: user.id})
-        await Category.createOrUpdate({name: "default"},{
-            name: 'default',income,outcome,userId
+        const exists = await Category.exists({name,userId: user.id})
+        if (!exists) {
+            return res.status(400).json({status: 'error',message: 'invalid category name'})
+        }
+        const {incomes,outcome,userId,_id} = await Category.findOne({name,userId: user.id})
+        await Category.updateOne({name: "default"},{
+            name: 'default',incomes,outcome,userId
         })
-        await Category.findByIdAndDelete(category._id)
-        return res.status(200).json({status: 'ok',message: 'category name updated'})
+        await Category.findByIdAndDelete(_id)
+        return res.status(200).json({status: 'ok',message: `category with name ${name} deleted succesfully`})
     } catch(error) {
         return res.status(500).json({status: 'error',message: 'internal server error'})
     }
 })
 
 app.put('/update-category-transactions',async (req,res) => {
-    const data = req.body
-    const user = jwb.verify(token,JWBSECRET)
+    const {data,token} = req.body
+    let user
+    try {
+        user = jwb.verify(token,JWBSECRET)
+    } catch(error) {
+        return res.status(400).json({status: 'error',message: 'invalid token format'})
+    }
     if (data.length === 0) {
-        return res.status(422).json({status: 'error',message: 'update data not found'})
+        return res.status(400).json({status: 'error',message: 'update data not found'})
     }
     try {
         data.forEach(async (item) => {
-            const {name,incomes,outcome} = item
+            const {name,incomes,outcomes} = item
             if (!name) {
+                const exists = await Category.findOne({name: "default",userId: user.id})
+                if(!exists) {
+                    await Category.create({name:"default",userId: user.id})
+                }
                 await Category.updateOne({name: "default",userId: user.id},{
-                    $set: {incomes: {$push: {incomes: {$each: incomes}}},outcome}
-                })
+                    $push: { incomes: {$each: incomes}}, $push: {outcomes: {$each: outcomes}}
+                },{ runValidators: true })
             } else {
-                await Category.findOneAndUpdate({name,userId: user.id},{
-                    $set: {incomes: {$push: {incomes: {$each: incomes}}},outcome}
-                })
+                const category = await Category.findOneAndUpdate({name,userId: user.id},{
+                    $push: { incomes: {$each: incomes},outcomes: {$each: outcomes}}
+                },{ runValidators: true })
             }
         })
         return res.status(200).json({status: 'ok',message: 'category transactions updated'})
+    } catch(error) {
+        return res.status(500).json({status: 'error',message: 'internal server error'})
+    }
+})
+
+app.get('/outcomes/user/:id/category/:name',async (req,res) => {
+    const {id,name} = req.params
+    const {filter,startDate,endDate,minAmount,maxAmount} = req.query
+    try {
+        const category = await Category.findOne({name,userId: id})
+        if(!category) {
+            return res.status(400).json({status: "error",message: "invalid search query"})
+        }
+        if(filter) {
+            if (filter === "time") {   
+                const categories = await Category.find({
+                    date: {$gte: startDate,$lte: endDate}
+                }) 
+                return res.status(200).json({status: "ok",data: categories})
+            }
+            if(filter === "money") {
+                const categories = await Category.find({
+                    'outcomes.amount': { $gte: minAmount,$lte: maxAmount}
+                })
+                console.log(categories)
+                return res.status(200).json({status: "ok",data: categories})
+            }
+        } else {
+            return res.status(200).json({status: "ok",data: category.outcomes})
+        }
     } catch(error) {
         return res.status(500).json({status: 'error',message: 'internal server error'})
     }
